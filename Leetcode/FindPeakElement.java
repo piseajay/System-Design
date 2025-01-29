2d1affa6664e9d1a6ce93885dde47f2ecbf3fab1
Ajay Pise <ajaypise@Ajays-MacBook.local>
2025-01-29 21:07:28 +0000

    LC 29/1

---
+++ Leetcode/FindPeakElement.java
@@ -0,0 +1,26 @@
+public class FindPeakElement {
+
+    public static void main(String[] args) {
+        System.out.println(findPeakElement(new int[]{1, 2, 1, 3, 5, 6, 4}));
+    }
+
+    public static int findPeakElement(int[] nums) {
+        if (nums.length == 1) {
+            return 0;
+        }
+        if (nums.length == 2) {
+            return nums[0] > nums[1] ? 0 : 1;
+        }
+
+        if (nums[nums.length - 1] > nums[nums.length - 2]) {
+            return nums.length - 1;
+        }
+
+        for (int i = 1; i < nums.length - 1; i++) {
+            if (nums[i] > nums[i - 1] && nums[i] > nums[i + 1]) {
+                return i;
+            }
+        }
+        return 0;
+    }
+}
