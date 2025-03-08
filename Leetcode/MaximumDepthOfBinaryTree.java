5c10e63fca56a63655802f5e1bdc920b9a47a5c1
Ajay Pise <piseajay557@gmail.com>
2025-03-08 20:45:49 +0000

    LC 8/3

---
+++ Leetcode/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,41 @@
+package Leetcode;
+
+public class MaximumDepthOfBinaryTree {
+
+    public static void main(String[] args) {
+
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20, new TreeNode(15), new TreeNode(7));
+
+        System.out.println(new MaximumDepthOfBinaryTree().maxDepth(root));
+    }
+
+    public int maxDepth(TreeNode root) {
+
+        if (root == null) {
+            return 0;
+        }
+
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+
+        return Math.max(left, right) + 1;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
