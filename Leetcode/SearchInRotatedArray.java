e0a5b8d1e20b03f8a5317fd3e461e547405e0416
Ajay Pise <piseajay557@gmail.com>
2025-03-15 18:49:43 +0000

    LC 15/3

---
+++ Leetcode/SearchInRotatedArray.java
@@ -0,0 +1,36 @@
+package Leetcode;
+
+public class SearchInRotatedArray {
+
+    public static void main(String[] args) {
+        System.out.println(search(new int[]{4, 5, 6, 7, 0, 1, 2}, 0));
+    }
+
+    public static int search(int[] nums, int target) {
+
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
+
+            int mid = (low + high) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target <= nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (nums[mid] <= target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
