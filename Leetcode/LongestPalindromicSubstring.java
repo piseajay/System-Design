8dca399ee2deab2bd5dd6c8aaf2f1d726e9db062
Ajay Pise <piseajay557@gmail.com>
2025-03-24 22:23:31 +0000

    LC 24/3

---
+++ Leetcode/LongestPalindromicSubstring.java
@@ -0,0 +1,43 @@
+package Leetcode;
+
+public class LongestPalindromicSubstring {
+
+    public static void main(String[] args) {
+        System.out.println(longestPalindrome("bb"));
+    }
+
+    public static String longestPalindrome(String s) {
+
+        if (s == null || s.isEmpty()) {
+            return "";
+        }
+
+        int start = 0;
+        int end = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+
+            int len = Math.max(len1, len2);
+
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+
+        return s.substring(start, end + 1);
+    }
+
+    public static int expandAroundCenter(String s, int left, int right) {
+
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+
+        return right - left - 1;
+    }
+}
